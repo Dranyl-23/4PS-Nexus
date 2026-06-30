18ff8b3c778a53f8a73369f5a16470ce5309263e
Dranyl <Alfielynard23@gmail.com>
2026-06-30 22:00:57 +0800

    feat: Add user's exact Freighter wallet to admin whitelist

--- web/src/lib/auth.ts
+++ web/src/lib/auth.ts
@@ -4,4 +4,5 @@
 export const AUTHORIZED_ADMIN_WALLETS = [
   // Add your Freighter wallet public key here to gain access during the demo!
   'GDI4QZJXCRCPO6KONAXXYFSN3NEY73OVPSDDD7C2OPW5QYX2IDQB5GNJ',
+  'GBBUS2TCA4ICRAFJP5CQ7HQUA6S7MMWLZHXMZA3USSYZJ2WDK4KNHO56' // User's Freighter Address
 ];
